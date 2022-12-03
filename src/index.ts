9dd34e569da3343aeafd399b0b4e939740204f19
“Altarya DeFlammes„ <blazethecatdflammes@outlook.com>
2022-12-03 16:25:47 -0300

    paddle moving

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,17 @@ let ballX = canvas.width/2
 let ballY = canvas.height - 30
 let ballDX = 1
 let ballDY = -1
-let ballRadious = 10
+const ballRadious = 10
 let FPS = 0
 let frameCount = 0
 let frameLock = false
+const paddleH = 10
+const paddleW = 50
+let paddleX = (canvas.width - paddleW)/2
+let paddleY = canvas.height - paddleH
+let moveLeft = false
+let moveRight = false
+let paddleSpeed = 7
 
 function drawLoop() {
     if(frameLock == false) {
@@ -31,12 +38,25 @@ function drawLoop() {
         ctx.stroke()
         ctx.closePath()
 
+        //Draw Paddle
+        ctx.beginPath()
+        ctx.fillStyle = 'green'
+        ctx.fillRect(paddleX, paddleY, paddleW, paddleH)
+        ctx.closePath()
+
         //Edge colision check
         if (ballY + ballDY < ballRadious || ballY + ballDY > canvas.height - ballRadious) {
             ballDY = -ballDY
         }
         if (ballX + ballDX < ballRadious || ballX + ballDX > canvas.width - ballRadious) {
             ballDX = -ballDX
+        }
+
+        //Move check
+        if (moveRight && paddleX - paddleSpeed < canvas.width - paddleW) {
+            paddleX += paddleSpeed
+        } else if (moveLeft && paddleX + paddleSpeed > 0 + paddleW) {
+            paddleX -= paddleSpeed
         }
 
         //Move ball
@@ -52,6 +72,27 @@ function countFPS() {
     FPS = frameCount
     frameCount = 0
 }
+
+function keyDownH(e: KeyboardEvent) {
+    if (e.key == 'ArrowRight' || e.key == 'Right' || e.key == 'KeyD' || e.key == 'd') {
+        moveRight = true
+    }
+    if (e.key == 'ArrowLeft' || e.key == 'Left' || e.key == 'KeyA' || e.key == 'a') {
+        moveLeft = true
+    }
+}
+
+function keyUpH(e: KeyboardEvent) {
+    if (e.key == 'ArrowRight' || e.key == 'Right' || e.key == 'KeyD' || e.key == 'd') {
+        moveRight = false
+    }
+    if (e.key == 'ArrowLeft' || e.key == 'Left' || e.key == 'KeyA' || e.key == 'a') {
+        moveLeft = false
+    }
+}
+
+document.addEventListener('keydown', keyDownH, false)
+document.addEventListener('keyup', keyUpH, false)
 
 setInterval(countFPS, 1000)
 setInterval(drawLoop, 1000/60)
