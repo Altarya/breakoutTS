009c03f4c5562f55797102a506028aa078bfdf24
“Altarya DeFlammes„ <blazethecatdflammes@outlook.com>
2022-12-03 14:20:12 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto"
+      },
+    ],
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.mjs', '.mts'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'docs'),
+  },
+  plugins: [
+	]
+};
